8b3718b27b5257dbc98d0f08dcc7e91ce0adeeac
Sam Hay <snh68@yahoo.com>
2023-01-29 03:19:39 -0800

    first commit

---
+++ main.py
@@ -0,0 +1,21 @@
+from flask import Flask, request, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def render_home():
+    return render_template('index.html')
+
+@app.route('/', methods = ["POST"])
+def calculate():
+    if request.method == 'POST':
+        bill = request.form['bill']
+        tip = request.form['tip']
+        people = request.form['people']
+        print(f'Tip: {tip}')
+        tip_total = float(bill) * float(tip) / 100
+        result = round((float(bill)+tip_total)/float(people), 2)
+        return render_template('index.html', result = result)
+
+if __name__ == '__main__':
+    app.run(host = '0.0.0.0', port = 3000)
